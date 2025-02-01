50f5a484ac6f0dc7ff8414d9896f388c3f94612d
hafidzahm <hafizhahm123@gmail.com>
2025-02-01 08:49:54 +0700

    chore: tampilan prototype tabel transaksi & form tambah transaksi (hidden)

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('ADAAA');
+const buttonEdit = document.getElementById('button-edit') 
+function editTransaksi() {
+    console.log('buttonEdit');
+}
+
+buttonEdit.addEventListener('click', editTransaksi)
