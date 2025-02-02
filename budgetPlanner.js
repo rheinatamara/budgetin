9f34406b20dddf527f79fd443480607556d05a55
rheinatamara <rheina.tamara@outlook.com>
2025-02-02 15:01:38 +0700

    add: budget planner

---
+++ budgetPlanner.js
@@ -0,0 +1,74 @@
+
+const options = {
+  chart: {
+    height: "90%",
+    maxWidth: "100%",
+    type: "area",
+    fontFamily: "Inter, sans-serif",
+    dropShadow: {
+      enabled: false,
+    },
+    toolbar: {
+      show: false,
+    },
+  },
+  tooltip: {
+    enabled: true,
+    x: {
+      show: false,
+    },
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      opacityFrom: 0.55,
+      opacityTo: 0,
+      shade: "#1C64F2",
+      gradientToColors: ["#1C64F2"],
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    width: 3,
+  },
+  grid: {
+    show: false,
+    strokeDashArray: 4,
+    padding: {
+      left: 2,
+      right: 2,
+      top: 0
+    },
+  },
+  series: [
+    {
+      name: "Balance",
+      data: [6500, 6418, 6456, 6526, 6356, 6456],
+      color: "#1A56DB",
+    },
+  ],
+  xaxis: {
+    categories: ['01 February', '02 February', '03 February', '04 February', '05 February', '06 February', '07 February'],
+    labels: {
+      show: false,
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    show: false,
+  },
+}
+
+document.querySelectorAll(".area-chart").forEach((chartElement) => {
+    if (typeof ApexCharts !== "undefined") {
+        const chart = new ApexCharts(chartElement, options);
+        chart.render();
+    }
+});
