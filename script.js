523b725779d19b67ecffe2c82d0fc6b6db694810
hafidzahm <hafizhahm123@gmail.com>
2025-02-02 12:05:47 +0700

    initial logic for filtering data

--- script.js
+++ script.js
@@ -31,3 +31,31 @@ function add (event) {
 const buttonAdd = document.getElementById('buttonAddTransaction')
 buttonAdd.addEventListener('click', add)
 
+function filter (event) {
+    event.preventDefault()
+    const filterTransactionName = document.getElementById('filterTransactionName')
+    const filterTransactionIncome = document.getElementById('filterTransactionIncome')
+    const filterTransactionExpense = document.getElementById('filterTransactionExpense')
+    const transactionStartFromNominal = document.getElementById('transactionStartFromNominal')
+    const transactionUntilNominal = document.getElementById('transactionUntilNominal')
+    const filterTransactionCategory = document.getElementById('filterTransactionCategory')
+    const transactionStartFromDate = document.getElementById('transactionStartFromDate')
+    const transactionUntilDate = document.getElementById('transactionUntilDate')
+
+    let transactionType = ''
+    if(filterTransactionIncome.checked) {
+        transactionType = filterTransactionIncome.value
+    } else if (filterTransactionExpense.checked) {
+        transactionType = filterTransactionExpense.value
+    }
+    console.log(filterTransactionName.value,);
+    console.log(transactionType);
+    console.log(transactionStartFromDate.value);
+    console.log(transactionUntilDate.value);
+    console.log(transactionStartFromNominal.value);
+    console.log(transactionUntilNominal.value);
+    console.log(filterTransactionCategory.value);
+}
+
+const buttonFilter = document.getElementById('transactionFilterButton')
+buttonFilter.addEventListener('click', filter)
