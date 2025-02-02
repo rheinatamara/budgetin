de780167b9173e02f32cf92557f2827b704dc62b
hafidzahm <hafizhahm123@gmail.com>
2025-02-02 11:51:53 +0700

    initial logic for add form data

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 console.log('ADAAA');
-const buttonEdit = document.getElementById('button-edit') 
-function editTransaksi() {
-    console.log('buttonEdit');
+// const buttonEdit = document.getElementById('button-edit') 
+// function editTransaksi() {
+//     console.log('buttonEdit');
+// }
+
+// buttonEdit.addEventListener('click', editTransaksi)
+function add (event) {
+    event.preventDefault()
+    const transactionName = document.getElementById('transactionName')
+    const transactionIncome = document.getElementById('transactionIncome')
+    const transactionExpense = document.getElementById('transactionExpense')
+    const transactionDate = document.getElementById('transactionDate')
+    const transactionNominal = document.getElementById('transactionNominal')
+    const transactionCategory = document.getElementById('transactionCategory')
+    const transactionNote = document.getElementById('transactionNote')
+    let transactionType = ''
+    if(transactionIncome.checked) {
+        transactionType = transactionIncome.value
+    } else if (transactionExpense.checked) {
+        transactionType = transactionExpense.value
+    }
+    console.log(transactionName.value,);
+    console.log(transactionType);
+    console.log(transactionDate.value);
+    console.log(transactionNominal.value);
+    console.log(transactionCategory.value);
+    console.log(transactionNote.value);
 }
 
-buttonEdit.addEventListener('click', editTransaksi)
+const buttonAdd = document.getElementById('buttonAddTransaction')
+buttonAdd.addEventListener('click', add)
+
