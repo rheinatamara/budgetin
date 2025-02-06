ee040585e632e50858c238c282af359b25a1da26
rheinatamara <rheina.tamara@outlook.com>
2025-02-06 12:24:00 +0700

    switch to indonesian

--- budgetPlanner.js
+++ budgetPlanner.js
@@ -234,13 +234,13 @@ function showDataCards(arr) {
           </div>
           <div class="mb-4">
             <p class="py-3 font-bold text-2xl text-gray-700">${formatIDR(data["remainingAmount"])}</p>
-            <p class="font-normal text-gray-400">of ${formatIDR(data["goalAmount"])}</p>
+            <p class="font-normal text-gray-400">dari ${formatIDR(data["goalAmount"])}</p>
           </div>
           <div class="w-full bg-gray-200 rounded-full h-1.5 mb-6">
             <div class="bg-blue-600 h-1.5 rounded-full" style="width: ${data["percentage"]}%"></div>
           </div>
           <button class="detailButton inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300" data-id="${data["id"]}">
-            More details
+            Lebih detail
             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
